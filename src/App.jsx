24324c0c520bfd56d9bb78ddc75cb90710141014
Hakim AZIZI <hakazizi@hotmail.com>
2024-03-27 16:22:08 +0100

    first commit to pokedex

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import PokemonCard from "./components/PokemonCard.jsx";
+// import Gallery from './Gallery.js';
+
+function App() {
+  return (<div><PokemonCard /></div>  );
+}
+
+export default App;
