2d74fcbe5f5bf07e1d286b6e7ceea29f2a887f4a
Hakim AZIZI <hakazizi@hotmail.com>
2024-03-28 00:10:15 +0100

    boucle for sur le tableau + condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import PokemonCard from "./components/PokemonCard.jsx";
 import './App.css';
-
 function App() {
-  return (<div><PokemonCard /></div>  );
+  return (<div  className="flex-show"><PokemonCard /></div>  );
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,28 @@
 
 function PokemonCard() {
+    const pokemon=[];
+    const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
+      for(let i=0;i<pokemonList.length;i++){
+        if(pokemonList!==undefined){
+            pokemon.push(pokemonList[i])
+        }
+      }
     return (
-    <><div className="flex-show">
-    <figure className="card">
-        <img className="car-img" src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' alt="Bulbasaur" />
+    <>
+     <figure className="card">
+        <img className="car-img" src={pokemon[0].imgSrc} />
     </figure>
     <figcaption className="text-card">
-        Bulbasaur
+    {pokemon[0].name}
         </figcaption>
-        </div>
     </>
     );
   }
