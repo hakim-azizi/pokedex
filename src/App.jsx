50a0351216d4472fc21f174b4ca4cb4e594507c5
Hakim AZIZI <hakazizi@hotmail.com>
2024-03-30 00:57:09 +0100

    tout fonctionne :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import PokemonCard from "./components/PokemonCard.jsx";
 import './App.css';
+
 function App() {
-  return (<div  className="flex-show"><PokemonCard /></div>  );
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
+
+
+
+
+
+
+
+  return (
+    
+
+    
+    < PokemonCard pokemon={pokemonList[0]} />
+    );
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,23 @@
+function PokemonCard(props) {
+  console.log("props : ",props);
 
-function PokemonCard() {
-    let pokemon=[,];
-    const pokemonList = [
-        {
-          name: "bulbasaur",
-          imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-          name: "mew",
-        },
-      ];
-      pokemonList[1].imgSrc ?  pokemon[0]=<img className="card-img" src={pokemonList[0].imgSrc} /> : pokemon[0]=<p>??????????</p>;
-      pokemonList[1].imgSrc ?  pokemon[1]=pokemonList[0].name : pokemon[1]=pokemonList[1].name;
+   let affiche;
+
+    props.pokemon.imgSrc ?  affiche=<img className="card-img" src={props.pokemon.imgSrc} /> : affiche=<p>??????????</p>;
+   // props.pokemon.imgSrc ?  affiche[1]=props.pokemon.name : affiche[1]=props.pokemon[1].name;
+
+
     return (
     <>
-    {console.log('tableau pokemon ',pokemon)}
+
     <figure className="card">
-      {pokemon[0]}
+    {affiche}
     </figure>
     <figcaption className="text-card">
-      {pokemon[1]}
+     {props.pokemon.name}
     </figcaption>
     </>
     );
   }
-  
+
   export default PokemonCard;
