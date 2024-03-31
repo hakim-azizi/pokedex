65d8dcb46b530ef1507595a6258aa41f6f69c45b
Hakim AZIZI <hakazizi@hotmail.com>
2024-03-31 22:21:50 +0200

    first committ

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import './App.css';
 function App() {
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      // imgSrc:0,
-    },
-    {
-      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-  ];
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
   return (
-      < PokemonCard pokemon={pokemonList[0]} />
+      < PokemonCard pokemon={pokemonList[3]} />
     );
 }
 export default App;
